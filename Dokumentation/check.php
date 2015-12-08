497aa3e9e645a771c35407ab6338789827cd72b6
AnhMinhNguyen <minh.blue.9194@gmail.com>
2015-12-08 23:58:27 +0100

    Create check.php

---
+++ Dokumentation/check.php
@@ -0,0 +1,8 @@
+<?php
+echo 'Frage: '. $_POST['frage'];
+echo '<br/>Antwort: '. $_POST['antwort'];
+?>
+<form action="#" method="post">
+    <input type="submit" value="Noch eine Frage hinzufügen" />
+    <input type="submit" value="Abfragen" />
+</form>
