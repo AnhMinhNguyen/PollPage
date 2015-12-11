3a7c99a968ec1c633027c3e82a1ffc11a33a0097
Markus Alpers <markus.alpers@haw-hamburg.de>
2015-12-11 09:55:04 +0100

    Feedback Alpers

--- Dokumentation/check.php
+++ Dokumentation/check.php
@@ -6,3 +6,9 @@ echo '<br/>Antwort: '. $_POST['antwort'];
     <input type="submit" value="Noch eine Frage hinzufügen" />
     <input type="submit" value="Abfragen" />
 </form>
+
+<!--
+=== Feedback Alpers, Dez 11 ===
+
+Meine Aufgabe besteht darin, Sie dabei zu unterstützen, Ihre Programmierfähigkeit zu verbessern.
+Doch dafür müssen Sie auch anfangen zu programmieren.
